5e03804e2780a47b096fc3f0d97a4f10b6ece18d
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-06-05 21:35:05 +1000

    Stuff

--- server/src/server/Server.java
+++ server/src/server/Server.java
@@ -4,6 +4,19 @@
  */
 package server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author caleb
@@ -14,7 +27,23 @@ public class Server {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int port = 2002;
+        try {
+            ServerSocket ss = new ServerSocket(port);
+            Socket s = ss.accept();
+            InputStream is = s.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            String to = (String) ois.readObject();
+            if (to != null) {
+                System.out.println(to);
+            }
+            
+            
+            is.close();
+            s.close();
+            ss.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-    
 }
